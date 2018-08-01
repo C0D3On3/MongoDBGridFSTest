b8ed7fcffebaeb6cd3f43753a44a84dc1d367424
Ryan A. Joseph <Ryan.VI.Joseph@gmail.com>
2018-07-31 20:13:19 -0400

    Test project

---
+++ MongoGridFSTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver.GridFS;
+
+namespace MongoGridFSTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public MongoGridFS gridFS { get; set; }
+        }
+    }
+}
